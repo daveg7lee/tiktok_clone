2fc8569cf20544d95dd7736a171f703849d08289
daveg7lee <daveg7lee@gmail.com>
2023-01-14 16:14:38 +0900

    #4.0 Sign Up Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -15,10 +15,7 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Padding(
-        padding: const EdgeInsets.all(Sizes.size14),
-        child: Container(),
-      ),
+      home: const SignUpScreen(),
     );
   }
 }
