b17741e14e37a81ae367e5c7b2d5ee2160663814
daveg7lee <daveg7lee@gmail.com>
2023-01-14 15:39:21 +0900

    #3.1 Constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -14,7 +15,10 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(),
+      ),
     );
   }
 }
