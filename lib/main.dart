469014511ad2437c62f9f5f6ca8d9ba91fec57fe
daveg7lee <daveg7lee@gmail.com>
2023-02-15 23:39:33 +0900

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class TikTokApp extends StatelessWidget {
               fontWeight: FontWeight.w600,
             )),
       ),
-      home: const MainNavigationScreen(),
+      home: MainNavigationScreen(),
     );
   }
 }
