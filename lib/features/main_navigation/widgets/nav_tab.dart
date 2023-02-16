b3cb42b4841766ae16056d2add20fae421f5a2c3
daveg7lee <daveg7lee@gmail.com>
2023-02-16 18:50:23 +0900

    Stateful Navigation part One

--- lib/features/main_navigation/widgets/nav_tab.dart
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -9,11 +9,12 @@ class NavTab extends StatelessWidget {
     required this.isSelected,
     required this.icon,
     required this.onTap,
+    required this.selectedIcon,
   });
 
   final String text;
   final bool isSelected;
-  final IconData icon;
+  final IconData icon, selectedIcon;
   final Function onTap;
 
   @override
@@ -30,7 +31,7 @@ class NavTab extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 FaIcon(
-                  icon,
+                  isSelected ? selectedIcon : icon,
                   color: Colors.white,
                 ),
                 Gaps.v5,
