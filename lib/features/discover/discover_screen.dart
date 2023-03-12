4f31f7bce0599805826964e51d18871be7a75450
daveg7lee <daveg7lee@gmail.com>
2023-03-12 19:21:12 +0900

    MediaQuery

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
@@ -45,6 +46,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
@@ -87,8 +89,14 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                   Sizes.size8,
                 ),
                 itemCount: 20,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: width > Breakpoints.lg
+                      ? 5
+                      : width > Breakpoints.md
+                          ? 4
+                          : width > Breakpoints.sm
+                              ? 3
+                              : 2,
                   crossAxisSpacing: Sizes.size8,
                   mainAxisSpacing: Sizes.size10,
                   childAspectRatio: 9 / 20,
