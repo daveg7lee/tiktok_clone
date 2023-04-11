fd8c2953889df2b5a05b03fd67fb4c2ef29134ed
daveg7lee <daveg7lee@gmail.com>
2023-04-11 23:24:15 +0900

    CameraController

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -9,8 +12,78 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+
+  late final CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController =
+        CameraController(cameras[0], ResolutionPreset.ultraHigh);
+
+    await _cameraController.initialize();
+
+    print(cameras);
+  }
+
+  Future<void> initPermission() async {
+    final cameraPermission = await Permission.camera.request();
+    final microphonePermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+
+    final microphoneDenied = microphonePermission.isDenied ||
+        microphonePermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !microphoneDenied) {
+      setState(() {
+        _hasPermission = true;
+      });
+      await initCamera();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermission();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Requesting permissions...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                  Gaps.v24,
+                  CircularProgressIndicator.adaptive()
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                ],
+              ),
+      ),
+    );
   }
 }
