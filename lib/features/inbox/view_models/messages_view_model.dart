a11e11ea1a8b3fd4127e4989419c55ef0aafdc46
daveg7lee <daveg7lee@gmail.com>
2023-05-15 21:22:39 +0900

    StreamProvider

--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication__repo.dart';
 import 'package:tiktok_clone/features/inbox/models/message.dart';
@@ -17,7 +18,11 @@ class MessagesViewModel extends AsyncNotifier<void> {
     final user = ref.read(authRepo).user;
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      final message = MessageModel(text: text, userId: user!.uid);
+      final message = MessageModel(
+        text: text,
+        userId: user!.uid,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+      );
       _repo.sendMessage(message);
     });
   }
@@ -25,3 +30,16 @@ class MessagesViewModel extends AsyncNotifier<void> {
 
 final messagesProvider =
     AsyncNotifierProvider<MessagesViewModel, void>(() => MessagesViewModel());
+
+final chatProvider = StreamProvider<List<MessageModel>>((ref) {
+  final db = FirebaseFirestore.instance;
+
+  return db
+      .collection("chat_rooms")
+      .doc("X2FHxKRXERRQpLrB5kvu")
+      .collection("texts")
+      .orderBy("createdAt")
+      .snapshots()
+      .map((event) =>
+          event.docs.map((doc) => MessageModel.fromJson(doc.data())).toList());
+});
